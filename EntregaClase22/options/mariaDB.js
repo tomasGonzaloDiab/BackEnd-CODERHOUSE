2f546c246a2960a9a25631d049c47cb93f53786c
tomasGonzaloDiab <tomasdiab@gmail.com>
2022-03-22 22:36:26 -0300

    entrega Clase22

---
+++ EntregaClase22/options/mariaDB.js
@@ -0,0 +1,13 @@
+const options = {
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'tonas',
+        password: 'tomas',
+        database: 'sqldb'
+    }
+}
+
+module.exports ={
+    options
+}
