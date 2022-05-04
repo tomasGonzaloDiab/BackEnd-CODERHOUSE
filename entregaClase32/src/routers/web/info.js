dc4c29dd974b0ddd6529b8e89b27474db48d83c8
tomasGonzaloDiab <tomasdiab@gmail.com>
2022-05-03 22:36:27 -0300

    entrega Clase32

---
+++ entregaClase32/src/routers/web/info.js
@@ -0,0 +1,30 @@
+const {Router} = require("express");
+const info = new Router();
+
+
+const { cpus } = require("os");
+
+
+const argumentosEntrada = process.argv;
+const pathEjec = process.cwd();
+const nomPlataforma = process.platform;
+const proccesId = process.pid;
+const vNode = process.version;
+const carpetaProyecto = process.cwd();
+const memReservada = process.memoryUsage();
+
+
+info.get("/info", (req, res) => {
+
+    res.render("info.hbs",{
+        argumentosEntrada: argumentosEntrada,
+        nomPlataforma: nomPlataforma,
+        vNode: vNode,
+        memReservada: memReservada,
+        pathEjec: pathEjec,
+        proccesId: proccesId,
+        carpetaProyecto: carpetaProyecto
+    }) 
+  });
+
+module.exports = info
